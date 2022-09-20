14d312d12f04d21eb61d7549b51c12109f471d0a
kamen <maks.kolesnikov.0202@mail.ru>
2022-09-20 14:38:38 +0300

    homework-variables-task-2

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,14 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+        //задача 4//
         var friend = 19;
         System.out.println(friend);
         friend = friend + 2;
         System.out.println(friend);
         friend = friend / 7;
         System.out.println(friend);
+        //задача 5//
         var frog = 3.5;
         System.out.println(frog);
         frog = frog * 10;
